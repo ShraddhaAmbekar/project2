47e28c9b954910ea024571c43caf8e9fc7ab9753
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-11-22 19:16:10 +0530

    routing practice

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ProjectsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
